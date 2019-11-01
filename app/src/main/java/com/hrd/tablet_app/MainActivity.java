9cf8ef3bfc7785e7f7625b4bf441fb67501cac63
Horacio <hrd712924@gmail.com>
2019-11-01 12:31:26 -0400

    Initial commit

---
+++ app/src/main/java/com/hrd/tablet_app/MainActivity.java
@@ -0,0 +1,144 @@
+package com.hrd.tablet_app;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create DatabaseHelper instance
+        DatabaseHelper dataHelper;
+        dataHelper = new DatabaseHelper(this);
+        // Insert sample data
+        dataHelper.insertData("Kent","HoReCa");
+        dataHelper.insertData("FineE","HoReCa");
+        dataHelper.insertData("MKent","GTrade");
+        dataHelper.insertData("MeviusLove","MTrade");
+        dataHelper.insertData("XEMo","HoReCa");
+        // Reference to TableLayout
+        TableLayout tableLayout=(TableLayout)findViewById(R.id.tabletData);
+        // Add header row
+        TableRow rowHeader = new TableRow(this);
+        rowHeader.setBackgroundColor(Color.parseColor("#c0c0c0"));
+        rowHeader.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+        String[] headerText={"ID","NAME","TYPE"};
+        for(String c:headerText) {
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            tv.setGravity(Gravity.CENTER);
+            tv.setTextSize(18);
+            tv.setPadding(5, 5, 5, 5);
+            tv.setText(c);
+            rowHeader.addView(tv);
+        }
+        tableLayout.addView(rowHeader);
+
+        // Get data from sqlite database and add them to the table
+        // Open the database for reading
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        // Start the transaction.
+        db.beginTransaction();
+
+        try
+        {
+            String selectQuery = "SELECT * FROM "+ DatabaseHelper.TABLE_OUTLET;
+            Cursor cursor = db.rawQuery(selectQuery,null);
+            if(cursor.getCount() >0)
+            {
+                while (cursor.moveToNext()) {
+                    // Read columns data
+                    int outlet_id= cursor.getInt(cursor.getColumnIndex("outlet_id"));
+                    String outlet_name= cursor.getString(cursor.getColumnIndex("outlet_name"));
+                    String outlet_type= cursor.getString(cursor.getColumnIndex("outlet_type"));
+
+                    // dara rows
+                    TableRow row = new TableRow(this);
+                    row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
+                    String[] colText={outlet_id+"",outlet_name,outlet_type};
+                    for(String text:colText) {
+                        TextView tv = new TextView(this);
+                        tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                                TableRow.LayoutParams.WRAP_CONTENT));
+                        tv.setGravity(Gravity.CENTER);
+                        tv.setTextSize(16);
+                        tv.setPadding(5, 5, 5, 5);
+                        tv.setText(text);
+                        row.addView(tv);
+                    }
+                    tableLayout.addView(row);
+
+                }
+
+            }
+            db.setTransactionSuccessful();
+
+        }
+        catch (SQLiteException e)
+        {
+            e.printStackTrace();
+
+        }
+        finally
+        {
+            db.endTransaction();
+            // End the transaction.
+            db.close();
+            // Close database
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
